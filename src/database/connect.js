bada292c6500cdfe7e91fce73350604e791be0d6
Everson Alves <76633641+Eversons99@users.noreply.github.com>
2023-08-10 14:21:34 -0300

    Fixing URL mongo

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import 'dotenv/config'
 
-mongoose.connect('mongodb://127.0.0.1:27017/newHouse', {
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.l49flcg.mongodb.net/newHouse`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
